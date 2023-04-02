abc96e460afbecb77bd7a3e4e5737451e1a437b2
MattPermentier <75488878+MattPermentier@users.noreply.github.com>
2023-04-02 13:07:21 +0200

    first commit

---
+++ train.js
@@ -0,0 +1,56 @@
+function loadData() {
+  Papa.parse("./data/winequality.csv", {
+    download: true,
+    header: true,
+    dynamicTyping: true,
+    complete: (results) => prepareData(results.data),
+  });
+}
+
+function prepareData(data) {
+  const nn = ml5.neuralNetwork({ task: "regression", debug: true });
+
+  data.sort(() => Math.random() > 0.5);
+
+  let trainData = data.slice(0, Math.floor(data.length * 0.8));
+  let testData = data.slice(Math.floor(data.length * 0.8) + 1);
+
+  for (let row of trainData) {
+    nn.addData(
+      {
+        fixed: row.fixedAcidity,
+        volatile: row.volatileAcidity,
+        citric: row.citricAcid,
+      },
+      { quality: row.quality }
+    );
+  }
+
+  for (let row of trainData) {
+    nn.addData(
+      {
+        fixed: row["fixedAcidity"],
+        volatile: row["volatileAcidity"],
+        citric: row["citricAcid"],
+        chlorides: row["chlorides"],
+        sulfur: row["freeSulfurDioxide"],
+        totalSulfer: row["totalSulfurDioxide"],
+        sulphates: row["sulphates"],
+        alcohol: row["alcohol"],
+      },
+      { quality: row.quality }
+    );
+  }
+
+  nn.normalizeData();
+  nn.train({ epochs: 5 });
+
+  let saveBtn = document.getElementById("saveBtn");
+  saveBtn.addEventListener("click", () => saveModel(nn));
+}
+
+function saveModel(nn) {
+  nn.save();
+}
+
+loadData();
